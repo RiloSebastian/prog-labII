03e200642042baa792105c2cadcd077cbd1a6431
Sebastian Rilo <36234060+S3baRr00@users.noreply.github.com>
2019-07-11 17:27:20 -0300

    Add files via upload

---
+++ 20190627_2doParcial_2C/ComiqueriaApp/ComiqueriaApp/ModificarProductoForm.cs
@@ -0,0 +1,99 @@
+﻿using ComiqueriaLogic;
+using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace ComiqueriaApp
+{
+    public partial class ModificarProductoForm : Form
+    {
+        private Producto productoSeleccionado;
+
+        /// <summary>
+        /// Constructor. 
+        /// </summary>
+        /// <param name="productoSeleccionado">Producto seleccionado por el usuario en la lista del principalForm.</param>
+        public ModificarProductoForm(Producto productoSeleccionado)
+        {
+            InitializeComponent();
+            this.productoSeleccionado = productoSeleccionado;
+        }
+
+        /// <summary>
+        /// Manejador del evento OnLoad del formulario. 
+        /// Inicializará el lblDescripción y el lblPrecioActual.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void VentasForm_Load(object sender, EventArgs e)
+        {
+            this.lblDescripcion.Text = this.productoSeleccionado.Descripcion;
+            this.txtPrecioActual.Text = String.Format("{0:0.00}", this.productoSeleccionado.Precio);
+        }
+
+        /// <summary>
+        /// Manejador del evento OnClick del btnModificar. 
+        /// Actualiza el precio del producto seleccionado en la base de datos. 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnModificarClick(object sender, EventArgs e)
+        {
+            double nuevoPrecio;
+            if (Double.TryParse(this.txtNuevoPrecio.Text, out nuevoPrecio))
+            {
+                DialogResult result = MessageBox.Show("¿Seguro desea modificar el producto?", "Modificar Producto", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                if (result == DialogResult.Yes)
+                {
+                    // Punto 4B - Actualizar el precio del producto en la tabla de productos. 
+                    SqlCommand command = new SqlCommand();
+                    SqlConnection connection = new SqlConnection(@"Data Source =.\SQLEXPRESS; Initial Catalog =Comiqueria; Integrated Security = True");
+                    try
+                    {
+                        command.CommandType = System.Data.CommandType.Text;
+                        command.Connection = connection;
+                        command.CommandText = string.Format("UPDATE Comiqueria SET Precio ='{0}' WHERE codigo = {1}", nuevoPrecio, productoSeleccionado.Codigo);
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Error de conexion de servidor", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                    this.Close();
+                }
+            }
+            else
+            {
+                this.lblError.Text = "Error. Debe ingresar un precio válido.";
+            }   
+        }
+
+        /// <summary>
+        /// Manejador del evento click del botón cancelar.
+        /// Cierra el formulario.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnCancelarClick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// Manejador del evento OnChange del txtNuevoPrecio. 
+        /// Limpiará el lblError.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtNuevoPrecio_TextChanged(object sender, EventArgs e)
+        {
+            this.lblError.Text = String.Empty;
+        }
+    }
+}
---
+++ 20190627_2doParcial_2C/ComiqueriaApp/ComiqueriaLogic/ComiqueriaDAO.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ComiqueriaLogic
+{
+    public static class ComiqueriaDAO
+    {
+        public static SqlCommand command;
+        public static SqlConnection connection;
+
+        static ComiqueriaDAO()
+        {
+            try
+            {
+                SqlCommand command = new SqlCommand();
+                SqlConnection connection = new SqlConnection(@"Data Source =.\SQLEXPRESS; Initial Catalog =Comiqueria; Integrated Security = True");
+                command.CommandType = System.Data.CommandType.Text;
+                command.Connection = connection;
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public static bool AgregarPaquete(string nuevaDescripcion, double nuevoPrecio, int nuevoStock)
+        {
+            bool retorno = false;
+            try
+            {
+                command.CommandText = string.Format("INSERT INTO Productos (Descripcion, Precio, Stock) VALUES ('{0}','{1}','{2}')",
+                                                nuevaDescripcion, nuevoPrecio, nuevoStock);
+                connection.Open();
+                command.ExecuteNonQuery();
+                retorno= true;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                }     
+            }
+            return retorno;
+        }
+
+        public static bool EliminarProducto(Producto productoAEliminar)
+        {
+            bool retorno = false;
+            try
+            {
+                command.CommandText = string.Format("DELETE FROM Productos WHERE codigo = {0}", productoAEliminar.Codigo);
+                connection.Open();
+                command.ExecuteNonQuery();
+                retorno = true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+            return retorno;
+        }
+
+        public static List<Producto> CargarListaDeBaseDeDatos()
+        {
+            List<Producto> productos;
+            Producto producto;
+
+            try
+            {
+                command.CommandText = "SELECT * FROM Productos";
+                connection.Open();
+                SqlDataReader odr = command.ExecuteReader();
+                productos = new List<Producto>();
+                
+                while (odr.Read())
+                {
+                    productos.Add(producto = new Producto(int.Parse(odr["Codigo"].ToString()), odr["Descripcion"].ToString(),
+                                                            int.Parse(odr["Stock"].ToString()), double.Parse(odr["Precio"].ToString())));
+                }
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+            return productos;
+        }
+
+
+    }
+}
+
